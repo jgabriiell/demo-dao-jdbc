1f827e2c342cc2aceacd8a8b9c58d640fa63d245
João Gabriel <jgabriiell@gmail.com>
2025-10-29 11:01:44 -0300

    Creating project and git repository

---
+++ src/db/DBIntegrityException.java
@@ -0,0 +1,13 @@
+package db;
+
+import java.io.Serial;
+
+public class DBIntegrityException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public DBIntegrityException(String msg) {
+        super(msg);
+    }
+}
