52340142eb81361515d31d3e4609bdadccd37fef
João Gabriel <jgabriiell@gmail.com>
2025-10-30 09:21:34 -0300

    Seller entity class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
 
         Department department = new Department(1, "Books");
         System.out.println(department);
+
+        Seller seller = new Seller(1, "John", "john@mgmail.com", LocalDate.now(), 1500d, department);
+        System.out.println(seller);
     }
 }
