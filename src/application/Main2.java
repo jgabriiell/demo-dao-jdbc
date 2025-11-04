756ed9ef167d3b4fcc32f2c816cc4036fa79da54
João Gabriel <jgabriiell@gmail.com>
2025-11-04 09:22:56 -0300

    DepartmentDaoJDBC and DaoFactory

---
+++ src/application/Main2.java
@@ -0,0 +1,11 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+
+public class Main2 {
+    public static void main(String[] args) {
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+        departmentDao.deleteById(1);
+    }
+}
---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,41 @@
+package model.dao.impl;
+
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+import java.sql.Connection;
+import java.util.List;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+
+    private final Connection connection;
+
+    public DepartmentDaoJDBC(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public void insert(Department department) {
+
+    }
+
+    @Override
+    public void update(Department department) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        System.out.println("Testing method");
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Department> findAll() {
+        return List.of();
+    }
+}
